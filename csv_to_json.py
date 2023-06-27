2a59b705b5ef13bd7f09e81a64edbadc9b0b3d64
Assassin <moinakdey.166@outlook.com>
2023-06-28 01:36:06 +0530

    updated the script to read any csv file and write the contents into json, removed js files

--- csv_to_json.py
+++ csv_to_json.py
@@ -1,47 +1,46 @@
 import csv
 import json
 
-def read_csv():
-    countries = []
-    with open('country_codes.csv', 'r') as country_code_csv:
-        csv_reader = csv.reader(country_code_csv)
-        for line in csv_reader:
-            countries.append(line)
-    return countries
+def read_csv(file):
+    data_set = []
+    with open(file, 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        for each_line in csv_reader:
+            data_set.append(each_line)
+    return data_set
 
 
-def csv_to_dict(countries):
-    country_dict_list = []
-    for country in countries:
-        country_dict = {
-            'country': country[0],
-            'code': country[1]
+def csv_to_dict(data_set):
+    dict_list = []
+    for data in data_set:
+        dict = {
+            data_set[0][0]: data[0],
+            data_set[0][1]: data[1]
         }
-        country_dict_list.append(country_dict)
-    return country_dict_list
+        dict_list.append(dict)
+    return dict_list
 
 
-def write_json_file(dict_list):
+def write_json_file(dict_list, file_name):
     i = 0
-    country_json_list = []
-    json_file = open('countries.json', 'w')
+    json_file = open(file_name, 'w')
     json_file.write('[')
     for dict in dict_list:
+        print(dict)
         i += 1
-        country_json = json.dumps(dict)
-        country_json_list.append(country_json)
+        json_ = json.dumps(dict)
         if i == len(dict_list):
-            json_file.writelines(f'{country_json}\n')
+            json_file.writelines(f'{json_}\n')
         else:
-            json_file.writelines(f'{country_json},\n')
+            json_file.writelines(f'{json_},\n')
     json_file.write(']')
     json_file.close()
 
 
-def csv_to_json():
-    countries = read_csv()
-    country_dict_list = csv_to_dict(countries)
-    write_json_file(country_dict_list)
+def csv_to_json(csv_file):
+    data_set = read_csv(csv_file)
+    dict_list = csv_to_dict(data_set)
+    write_json_file(dict_list[1:], 'countries.json')
 
 
-csv_to_json()
+csv_to_json('country_codes.csv')
